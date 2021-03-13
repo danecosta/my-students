ea04f0da038fe808edb4276a88f213cfdb383a4e
Daniela Cardoso da Costa <daniccosta92@gmail.com>
2021-03-13 17:10:57 -0300

    Form new student

--- src/app/components/student/student-new/student-new.component.ts
+++ src/app/components/student/student-new/student-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from '../models/student.model';
 
 @Component({
   selector: 'app-student-new',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentNewComponent implements OnInit {
 
+  tittle = 'New';
+  student = new Student();
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  saveStudent(): void { }
+
 
 }
